bffd825f7e04e30a4350d60dc2f921c1e415f338
Emil Eriksson <emil.eriksson@codemill.se>
2015-08-27 21:05:27 +0200

    Really ugly solution for context-menu

--- app/helpers/grids_helper.rb
+++ app/helpers/grids_helper.rb
@@ -1,2 +1,19 @@
 module GridsHelper
+  include Redmine::I18n
+
+  def grids_context_menu(url)
+    unless @context_menu_included
+      content_for :header_tags do
+        javascript_include_tag('context_menu') +
+          stylesheet_link_tag('context_menu')
+      end
+      if l(:direction) == 'rtl'
+        content_for :header_tags do
+          stylesheet_link_tag('context_menu_rtl')
+        end
+      end
+      @context_menu_included = true
+    end
+    #javascript_tag "contextMenuInit('#{ url_for(url) }')"
+  end
 end
