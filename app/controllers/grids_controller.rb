c418cbfd5c12011d215ce825f871c48ea4d6e55a
Marcus Karlsson <marcus.karlsson@codemill.se>
2015-09-29 12:13:32 +0200

    Show custom field values on the X axis
    Custom fields are handled differently from other fields so we need to
    make sure we handle them. The only way we can really detect if it is a
    custom field is by looking at its name.

--- app/controllers/grids_controller.rb
+++ app/controllers/grids_controller.rb
@@ -21,22 +21,17 @@ class GridsController < ApplicationController
     "fixed_version" => proc {|project| project.versions },
     "status" => proc {|project| IssueStatus.all },
   }
-  PROCS.default = proc do
-    puts("missing")
-    []
-  end
 
   def index
     retrieve_query
 
-    @attribute_options = ATTRIBUTES
-    # @issues = @project.issues
-
+    @attribute_options = get_attribute_options
+    @procs = PROCS.clone
     @issues = @query.issues()
-
     @horizontal = params[:horizontal]
     @vertical = params[:vertical]
     @sorting = params[:sorting]
+    @custom_field_values = {}
 
     issue = Issue.new
     valid_request = true
@@ -78,21 +73,31 @@ class GridsController < ApplicationController
     @y_items = create_items @vertical
 
     @issues.each do |issue|
+      # The important point here is to get the ID of x and y
       y = issue[@vertical]
       y = issue.send(@vertical) if y.nil?
-      #puts("#{@vertical.inspect} #{y.inspect}")
-
-      x = issue[@horizontal]
-      x = issue.send(@horizontal) if x.nil?
-      #puts("#{@horizontal.inspect} #{x.inspect}")
+      if @horizontal.start_with? "custom_field-"
+        # This is a custom field
+        value = nil
+        id = @horizontal.sub("custom_field-", "").to_i
+        issue.custom_field_values.each do |custom_field_value|
+          if custom_field_value.custom_field_id == id
+            value = custom_field_value.to_s
+          end
+        end
+        x = @custom_field_values[id][value] unless value.nil?
+      else
+        x = issue[@horizontal]
+        x = issue.send(@horizontal) if x.nil?
+      end
       @data[get_id y][get_id x].push(issue)
     end
   end
 
   private
 
-  def valid_param?(parameter)
-    PROCS.has_key?(parameter) and Issue.attribute_method?(parameter)
+  def valid_param? name
+    (@procs.has_key? name and Issue.attribute_method? name) or name.start_with? "custom_field-"
   end
 
   def find_project
@@ -102,8 +107,17 @@ class GridsController < ApplicationController
   def create_items name
     items = []
 
-    PROCS[name].call(@project).to_a.each do |x|
-      items << {:id => get_id(x), :name => x.name}
+    if @procs.has_key? name
+      @procs[name].call(@project).to_a.each do |x|
+        items << {:id => get_id(x), :name => x.name}
+      end
+    elsif name.start_with? "custom_field-"
+      # This is a custom field
+      id = name.sub("custom_field-", "").to_i
+      @custom_field_values[id] = get_custom_field_values id
+      @custom_field_values[id].each do |value, value_id|
+        items << {:id => value_id, :name => value}
+      end
     end
 
     unless is_value_required name then
@@ -136,6 +150,8 @@ class GridsController < ApplicationController
     case entity
     when nil
       :none
+    when Fixnum
+      entity
     when Member
       entity.user_id
     else
@@ -149,5 +165,28 @@ class GridsController < ApplicationController
 
   def should_sort_by_name item
     %w[assigned_to fixed_version].include? item
+  end
+
+  def get_attribute_options
+    attribute_options = ATTRIBUTES.clone
+    @project.all_issue_custom_fields.each do |custom_field|
+      attribute_options << [custom_field[:name], "custom_field-#{custom_field[:id]}"]
+    end
+    attribute_options
+  end
+
+  def get_custom_field_values id
+    values = {}
+    value_id = 1
+    @issues.each do |issue|
+      issue.custom_field_values.each do |custom_field_value|
+        if custom_field_value.custom_field_id == id
+          value = custom_field_value.to_s
+          values[value] = value_id unless (value.empty? or values.has_key? value)
+          value_id += 1
+        end
+      end
+    end
+    values
   end
 end
